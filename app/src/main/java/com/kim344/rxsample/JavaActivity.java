8167a5b5a309942b7b2323e2ab4b2e7956119c52
kim344 <dongju8907@gmail.com>
2020-01-28 23:35:28 +0900

    - List Example

--- app/src/main/java/com/kim344/rxsample/JavaActivity.java
+++ app/src/main/java/com/kim344/rxsample/JavaActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import io.reactivex.Observable;
@@ -13,6 +15,9 @@ import io.reactivex.ObservableEmitter;
 import io.reactivex.disposables.Disposable;
 
 public class JavaActivity extends AppCompatActivity {
+
+    static String TAG = "Confirm";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +49,15 @@ public class JavaActivity extends AppCompatActivity {
         String[] arr = {"A","B","C"};
         Observable<String> arrSource = Observable.fromArray(arr);
         arrSource.subscribe(arrData -> Log.e("Confirm fromArray",arrData));
+
+
+
+        List<String> names = new ArrayList<>();
+        names.add("Tom");
+        names.add("Kim");
+        names.add("Park");
+        Observable<String> listSource = Observable.fromIterable(names);
+        listSource.subscribe(listData -> Log.e("Confirm Iterator",listData));
     }
 
 }
